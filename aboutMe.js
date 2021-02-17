d25b7e8d42de7bcb2ef8b04fffb361fc6a4aa257
Tamyres Portilho <tamyportilho@yahoo.com.br>
2021-02-16 23:40:27 -0500

    Create aboutMe.js

---
+++ aboutMe.js
@@ -0,0 +1,55 @@
+// My full name
+const firstName = "Tamyres";
+const middleName = "Priscila";
+const lastName = "da Silva Portilho";
+let nickName = "Tamy";
+
+console.log(
+  `Hi, my name is ${firstName} ${middleName} ${lastName}, but everyone calls me ${nickName}.`
+);
+
+// About me
+const myAge = 24;
+const driversLisenceAge = 24 - 18;
+const degreeAge = 21;
+
+console.log(
+  `I am ${myAge} years old, so I started to drive ${driversLisenceAge} years ago and I got my Pedagogy degree (Early Child Education) when I was ${degreeAge} years old.`
+);
+
+// Personality
+const myPersonality = [
+  "kind",
+  " loyal",
+  " responsible",
+  " communicative person",
+  " open minded",
+  " dedicated",
+  " careful",
+  " affectionate",
+  " and great driver",
+];
+
+console.log(`A little about me, I am ${myPersonality}.`);
+
+// My skills
+let computerSkills = ["JavaScript", " HTML5", " and CSS3"];
+let musicSkills = ["play organ", " and violin"];
+let othersSkills = ["Brazilian Sign Language", " take care of children"];
+
+console.log(
+  `I am learning and improving in ${computerSkills}, also I know how to ${musicSkills} and I have some certificates in ${othersSkills}.`
+);
+
+// Hobbies
+let myHobbies = [
+  "Traveling",
+  " surfing the computer",
+  " listening to music",
+  " playing violin or organ",
+  " waching shows and movies",
+  " making some exercises and walking",
+  " and being with my family",
+];
+
+console.log(`Here are some hobbies that I like to do ${myHobbies}.`);
